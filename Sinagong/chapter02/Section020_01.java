424757cf0dfc68fe262a2348dd8dbdd2438b2768
Administrator <Administrator@roeuihyun-PC>
2016-04-11 00:01:00 +0900

    Sinagong Section020_01 finish

--- Sinagong/chapter02/Section020_01.java
+++ Sinagong/chapter02/Section020_01.java
@@ -6,7 +6,6 @@ package chapter02;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 /**
  * @author Administrator
@@ -18,6 +17,9 @@ public class Section020_01 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+		System.out.println("2진수 5자리를 입력하세요.");
+		System.out.println("맨 처음 수가 1이면 그레이코드고 맨 처음수가 0이면 2진수 입니다.");
 		
 		//1.입력된 자료와 변환된 자료가 저장될 배열을 선언한다.
 		BufferedReader systemIn = new BufferedReader(new InputStreamReader(System.in));
@@ -35,7 +37,8 @@ public class Section020_01 {
 		//3.변환할 자료를 입력 받는다.
 		INPUT = n.toString().toCharArray();
 		
-		//4.첫번째로 변환되는 비트를 옮긴다. 첫 번째 비트는 이진수나 그레이 코드에 관계없이 입력받은 자료를 그대로 옮기며, 실제 데이터는 입력받은 배열의 두 번째부터라는 것을 염두에 두고 생각한다.
+		//4.첫번째로 변환되는 비트를 옮긴다. 첫 번째 비트는 이진수나 그레이 코드에 관계없이 입력받은 자료를 그대로 옮기며, 
+		//실제 데이터는 입력받은 배열의 두 번째부터라는 것을 염두에 두고 생각한다.
 		CONT[0] = INPUT[1];
 		
 		//5.입력받은 자료의 첫 번째 비트가 1이면 이진수로 변환하는 것이고 아니면 그레이 코드로 변환하기 위해 else로 간다.
@@ -44,9 +47,13 @@ public class Section020_01 {
 			for(int i = 0; i < 3 ; i++){
 				
 				if(INPUT[i+2] == CONT[i]){
-					CONT[i+1] = new String("0").charAt(0); 
+					
+					CONT[i+1] = new String("0").charAt(0);
+					
 				}else{
-					CONT[i+1] = new String("1").charAt(0);;
+					
+					CONT[i+1] = new String("1").charAt(0);
+					
 				}
 				
 			}
@@ -56,25 +63,30 @@ public class Section020_01 {
 			for(int i = 0; i < 3 ; i++){
 				
 				if(INPUT[i+1] == INPUT[i+2]){
-					CONT[i+1] = new String("0").charAt(0);; 
+					
+					CONT[i+1] = new String("0").charAt(0);
+					
 				}else{
-					CONT[i+1] = new String("1").charAt(0);;
+					
+					CONT[i+1] = new String("1").charAt(0);
+					
 				}
 			
 			}
 			
 		}
 		
-		
-		if(INPUT[0]==1){
+		if(INPUT[0] == new String("1").charAt(0)){
+			
 			System.out.println("이진수");
+			
 		}else{
+			
 			System.out.println("그레이 코드");
+			
 		}
 		
-		
-		String CONTOUT = new String(CONT,0,CONT.length);
-		System.out.print(CONTOUT);
+		System.out.print(new String(CONT,0,CONT.length));
 		
 	}
 	
